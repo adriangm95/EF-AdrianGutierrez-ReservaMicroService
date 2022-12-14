28b93242a8470192a66a7bc95f9dccd1167caed9
adriangm95 <115050376+adriangm95@users.noreply.github.com>
2022-12-15 00:54:00 +1200

    Add files via upload

---
+++ insert_reserva.sql
@@ -0,0 +1,4 @@
+INSERT INTO evfinaldb.reserva VALUES (1,3500);
+INSERT INTO evfinaldb.reserva VALUES (2,2000);
+INSERT INTO evfinaldb.reserva VALUES (3,3400);
+INSERT INTO evfinaldb.reserva VALUES (4,4200);
